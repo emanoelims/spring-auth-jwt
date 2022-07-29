45754291a018eef0c3029e74ceafa6095feeb930
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2022-07-29 16:58:29 -0700

    feat: add role entity

---
+++ src/main/java/dev/emmanoel/springauthjwt/domain/Role.java
@@ -0,0 +1,41 @@
+package dev.emmanoel.springauthjwt.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = SEQUENCE)
+    private Long id;
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Role role = (Role) o;
+        return id != null && Objects.equals(id, role.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
