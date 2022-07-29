13afb04aba11d27a4fb1b55216865ac693b65026
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2022-07-29 16:59:00 -0700

    feat: add user entity

---
+++ src/main/java/dev/emmanoel/springauthjwt/domain/User.java
@@ -0,0 +1,49 @@
+package dev.emmanoel.springauthjwt.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Objects;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = SEQUENCE)
+    private Long id;
+    private String username;
+    private String password;
+
+    @ManyToMany
+    @ToString.Exclude
+    private Collection<Role> roles = new ArrayList<>();
+
+    public void addRole(Role role) {
+        roles.add(role);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        User user = (User) o;
+        return id != null && Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
