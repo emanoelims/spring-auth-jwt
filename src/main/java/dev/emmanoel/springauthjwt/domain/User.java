a4db524dffd98743763cf89a419b3692b52d21e0
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2022-07-29 17:50:32 -0700

    refactor: add constructor to name and password

--- src/main/java/dev/emmanoel/springauthjwt/domain/User.java
+++ src/main/java/dev/emmanoel/springauthjwt/domain/User.java
@@ -26,6 +26,11 @@ public class User {
     private String username;
     private String password;
 
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
     @ManyToMany
     @ToString.Exclude
     private Collection<Role> roles = new ArrayList<>();
